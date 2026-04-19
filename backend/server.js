8d8e8c15d407aa5bb8685fb10440c5e6a5b45435
tahertemraz <temraztaher@gmail.com>
2026-04-19 15:29:17 +0300

    Fix port for deployment

--- backend/server.js
+++ backend/server.js
@@ -27,8 +27,10 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
-    );
+    const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
   })
   .catch((err) => console.log(err));
